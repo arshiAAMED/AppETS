f4940c58155fbe7b56cb1bebbd0853c1ca9718ce
aahmed <1945514@crosemont.qc.ca>
2024-01-25 20:49:40 -0500

    App Template avec NavBar

---
+++ project1/lib/pages/page2.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Page2 extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Center(
+      child: Text(
+        'Likes',
+        style: TextStyle(fontSize: 50),
+      ),
+    );
+  }
+}
