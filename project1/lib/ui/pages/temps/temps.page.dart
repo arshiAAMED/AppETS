36ff83b41ae365bd259670d60086a4019ffaf711
Audric <audric.ligtas@gmail.com>
2024-01-27 17:12:31 -0500

    Page temp et settings front-end fini

--- project1/lib/ui/pages/temps/temps.page.dart
+++ project1/lib/ui/pages/temps/temps.page.dart
@@ -11,42 +11,142 @@ class _TempsPageState extends State<TempsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[300],
       appBar: AppBar(
-        title: const Text('Temps et Parametres'),
+        title: const Text('Temps et Parametres', style: TextStyle(fontSize: 35),),
         centerTitle: true,
       ) ,
       body: Center(
-          child:Container(
+        child: Align(
             alignment: Alignment.topCenter,
-              width:300,
-              height:100,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-                  boxShadow: [
-                  BoxShadow(
-                    color: Colors.red.shade400,
-                    offset: Offset(4.0,-270.0),
-                    //blurRadius: 5,
-                    spreadRadius: 1.0
+            child: Column(
+                children: [
+                  Container(
+                    width:300,
+                    height:100,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.red.shade400,
+                              offset: Offset(1.0,20.0),
+                              //blurRadius: 10,
+                              spreadRadius: 5.0
+                          ),
+                          const BoxShadow(
+                            color: Colors.white,
+                            offset: Offset(-4.0,15.0),
+                            //blurRadius: 6,
+                            spreadRadius: 1.0,
+                          )
+                        ]
+                    ),
+                    child: const Column(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          Text('12:00:00 AM/PM', style: TextStyle(fontSize: 20),),
+                          Text('Janvier 27 2024', style: TextStyle(fontSize: 20),),
+                        ]
+                    ),
                   ),
-                  const BoxShadow(
-                      color: Colors.white,
-                      offset: Offset(-4.0,-280.0),
-                      //blurRadius: 6,
-                      spreadRadius: 1.0
+                  Container(
+                    //color: Colors.red[400],
+                    alignment: Alignment.bottomCenter,
+                      height: 553,
+                      child: Padding(
+                        padding: EdgeInsets.all(10.0),
+                          child: Column(
+                            //Par defaut : crossAxisAlignment: CrossAxisAlignment.center,
+                            //Padding add
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+
+                                Text('Settings', style: TextStyle(fontSize: 45)),
+                                ElevatedButton(onPressed: (){
+                                },
+                                    style: ElevatedButton.styleFrom(
+                                      shadowColor: Colors.red.shade400
+                                    ),
+                                  child: const Padding(
+                                    padding: EdgeInsets.fromLTRB(50.0, 1.0, 50.0, 1.0),
+                                    child: Text('Profil'),
+                                  )
+                                ),
+                                ElevatedButton(onPressed: (){
+                                },
+                                    style: ElevatedButton.styleFrom(
+                                        shadowColor: Colors.red.shade400
+                                    ),
+                                    child: const Padding(
+                                      padding: EdgeInsets.fromLTRB(40.0, 1.0, 40.0, 1.0),
+                                      child: Text('Langage'),
+                                    )
+                                ),
+                                ElevatedButton(onPressed: (){
+                                },
+                                    style: ElevatedButton.styleFrom(
+                                        shadowColor: Colors.red.shade400
+                                    ),
+                                    child: const Padding(
+                                      padding: EdgeInsets.fromLTRB(47.0, 1.0, 47.0, 1.0),
+                                      child: Text('Theme'),
+                                    )
+                                ),
+                                ElevatedButton(onPressed: (){
+                                },
+                                    style: ElevatedButton.styleFrom(
+                                        shadowColor: Colors.red.shade400
+                                    ),
+                                    child: const Padding(
+                                      padding: EdgeInsets.fromLTRB(40.0, 1.0, 40.0, 1.0),
+                                      child: Text('Favorites'),
+                                    )
+                                ),
+                                ElevatedButton(onPressed: (){
+                                },
+                                    style: ElevatedButton.styleFrom(
+                                        shadowColor: Colors.red.shade400
+                                    ),
+                                    child: const Padding(
+                                      padding: EdgeInsets.fromLTRB(40.0, 1.0, 40.0, 1.0),
+                                      child: Text('Location'),
+                                    )
+                                ),
+                                ElevatedButton(onPressed: (){
+                                },
+                                    style: ElevatedButton.styleFrom(
+                                        shadowColor: Colors.red.shade400
+                                    ),
+                                    child: const Padding(
+                                      padding: EdgeInsets.fromLTRB(45.0, 1.0, 45.0, 1.0),
+                                      child: Text('Privacy'),
+                                    )
+                                ),
+                                ElevatedButton(onPressed: (){
+                                },
+                                    style: ElevatedButton.styleFrom(
+                                        shadowColor: Colors.red.shade400
+                                    ),
+                                    child: const Padding(
+                                      padding: EdgeInsets.fromLTRB(30.0, 1.0, 30.0, 1.0),
+                                      child: Text('Notifications'),
+                                    )
+                                ),
+                                ElevatedButton(onPressed: (){
+                                },
+                                  style: ElevatedButton.styleFrom(
+                                      shadowColor: Colors.red.shade400
+                                  ),
+                                  child: Text('Widget Customization'),
+                                ),
+                              ]
+                          )
+                      )
                   )
                 ]
-              ),
-              child:const Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Text('12:00:00 AM/PM', style: TextStyle(fontSize: 30),),
-                  Text('Janvier 27 2024', style: TextStyle(fontSize: 30),),
-                ],
-              )
-          )
+            )
+        ),
       ),
+
     );
   }
 }
