3da915ae092db26cb9f4b90353e4b1169c22d21a
Audric <audric.ligtas@gmail.com>
2024-01-27 15:00:16 -0500

    temps et date gabarit

--- project1/lib/ui/pages/temps/temps.page.dart
+++ project1/lib/ui/pages/temps/temps.page.dart
@@ -11,7 +11,42 @@ class _TempsPageState extends State<TempsPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("Temps page")),
+      backgroundColor: Colors.grey[300],
+      appBar: AppBar(
+        title: const Text('Temps et Parametres'),
+        centerTitle: true,
+      ) ,
+      body: Center(
+          child:Container(
+            alignment: Alignment.topCenter,
+              width:300,
+              height:100,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                  boxShadow: [
+                  BoxShadow(
+                    color: Colors.red.shade400,
+                    offset: Offset(4.0,-270.0),
+                    //blurRadius: 5,
+                    spreadRadius: 1.0
+                  ),
+                  const BoxShadow(
+                      color: Colors.white,
+                      offset: Offset(-4.0,-280.0),
+                      //blurRadius: 6,
+                      spreadRadius: 1.0
+                  )
+                ]
+              ),
+              child:const Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text('12:00:00 AM/PM', style: TextStyle(fontSize: 30),),
+                  Text('Janvier 27 2024', style: TextStyle(fontSize: 30),),
+                ],
+              )
+          )
+      ),
     );
   }
 }
